e4d17ed7ff5b9a16dd2c6da4bf7fd7577fd473d2
ButenkoA <andriybutenko94@gmail.com>
2019-10-23 08:11:33 +0300

    updated names

--- js.js
+++ js.js
@@ -1,28 +1,28 @@
 document.querySelector('#go').onclick = go;
 function go(){
-let point1 = [],point2 = [];
-point1.push(+(document.querySelector('#x1').value));
-point1.push(+(document.querySelector('#y1').value));
-point2.push(+(document.querySelector('#x2').value));
-point2.push(+(document.querySelector('#y2').value));
-let re=[];
+let departure = [],destination = [];
+departure.push(+(document.querySelector('#x1').value));
+departure.push(+(document.querySelector('#y1').value));
+destination.push(+(document.querySelector('#x2').value));
+destination.push(+(document.querySelector('#y2').value));
+let distance=[];
 for(let i=0;i<2;i++){
-    if(point1[i]<point2[i]){
-        re[i] = point2[i] - point1[i];
+    if(departure[i]<destination[i]){
+        distance[i] = destination[i] - departure[i];
     }else{
-        re[i] = point1[i] - point2[i];
+        distance[i] = departure[i] - destination[i];
     }
-    if(Math.floor(point1[i])==Math.floor(point2[i])){
-        if((point1[i]-Math.floor(point1[i]))+(point2[i]-Math.floor(point2[i]))>((Math.ceil(point1[i]))-point1[i])+((Math.ceil(point2[i]))-point2[i])){
-            re[i] = +((Math.ceil(point1[i])-point1[i])+(Math.ceil(point2[i])-point2[i]));
+    if(Math.floor(departure[i])==Math.floor(destination[i])){
+        if((departure[i]-Math.floor(departure[i]))+(destination[i]-Math.floor(destination[i]))>((Math.ceil(departure[i]))-departure[i])+((Math.ceil(destination[i]))-destination[i])){
+            distance[i] = +((Math.ceil(departure[i])-departure[i])+(Math.ceil(destination[i])-destination[i]));
         }else{
-            re[i] = +((point1[i]-Math.floor(point1[i]))+(point2[i]-Math.floor(point2[i])));
+            distance[i] = +((departure[i]-Math.floor(departure[i]))+(destination[i]-Math.floor(destination[i])));
         }
     }
 }
 let result=0;
-for(let i=0;i<re.length;i++){
-    result += re[i]
+for(let i=0;i<distance.length;i++){
+    result += distance[i]
 }
 document.getElementById('result').innerHTML ='The shortest way - '+result.toFixed(1);
 }
